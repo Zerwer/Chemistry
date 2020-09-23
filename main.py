61d37c1e3159244cddb791db66ec00ae78e0d267
Zerwer <robobug9@gmail.com>
2020-09-23 14:09:56 -0400

    LogP prediction to GUI

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import sys
 from PIL.ImageQt import ImageQt
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QLineEdit
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QValidator, QDoubleValidator
 from rdkit import Chem
-from rdkit.Chem import Draw
+from rdkit.Chem import Draw, rdMolDescriptors
+from chemical_models import AtomPairSolubility, LogP, LogPSolubility, CombinedSolubility
+
+# Load models
+logP_model = LogP('logP')
 
 
 class MainWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.label = QLabel()
+        self.mol_img = QLabel()
+        self.logP = QLabel()
 
         self.smiles = QLineEdit()
         self.smiles.returnPressed.connect(self.string_returned)
 
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.mol_img)
+        self.layout.addWidget(self.logP)
         self.layout.addWidget(self.smiles)
         self.setLayout(self.layout)
 
@@ -28,9 +34,12 @@ class MainWidget(QWidget):
         try:
             mol = Chem.MolFromSmiles(self.smiles.text())
             img = QImage(ImageQt(Draw.MolToImage(mol)))
-            self.label.setPixmap(QPixmap(img))
+            fp = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(mol)
+            logP = logP_model.run(fp)
+
+            self.logP.setText('LogP: ' + str(logP))
+            self.mol_img.setPixmap(QPixmap(img))
         except Exception as e:
-            print('Invalid SMILE string.')
             print(e)
 
 
